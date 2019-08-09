cc41ac27e15a57c4fd34869ce41a6c659063aebe
fanrenFRank <1778907544@qq.com>
2019-08-09 10:37:17 +0800

    sss

--- UMSocial/JBUMHandler/UMHandler.h
+++ UMSocial/JBUMHandler/UMHandler.h
@@ -13,8 +13,16 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UMHandler : NSObject
 +(UMHandler *)sharedInstance;
-
-
+/***  开始统计页面 */
++ (void)PageAnalaysBegain:(UIViewController*)vc;
+/***  结束统计页面 */
++ (void)PageAnalaysEnd:(UIViewController*)vc;
+/***  视频页面tab点击 */
++(void)eventVideoTabAnalys;
+/***  视频页面下载点击事件次数 */
++(void)eventVideoGameDownLoadTaps:(NSInteger)gameId andGameName:(NSString*)gameName;
+/***  视频页面下载安装次数 */
++(void)eventVideoGameInstalls:(NSInteger)gameId andGameName:(NSString*)gameName;
 @end
 
 NS_ASSUME_NONNULL_END
--- UMSocial/JBUMHandler/UMHandler.m
+++ UMSocial/JBUMHandler/UMHandler.m
@@ -20,12 +20,26 @@
     return _sharedInstance;
 }
 
-
++ (void)PageAnalaysBegain:(UIViewController*)vc{
+    [MobClick beginLogPageView:vc.className];
+}
++ (void)PageAnalaysEnd:(UIViewController*)vc{
+    [MobClick endLogPageView:vc.className];
+}
 +(void)eventVideoTabAnalys{
     static NSInteger count = 0;
     count++;
     [MobClick event:@"ManTab" attributes:@{@"tap" : @"视频主页按钮点击次数"} counter:(int)count];
 }
-
++(void)eventVideoGameDownLoadTaps:(NSInteger)gameId andGameName:(NSString*)gameName{
+    static NSInteger count = 0;
+    count++;
+    [MobClick event:@"eventVideoGameDownLoadTaps" attributes:@{@"tap" : @"视频页面下载按钮点击次数",@"gameName":gameName,@"gameId":[NSString stringWithFormat:@"%ld",(long)gameId]} counter:(int)count];
+}
++(void)eventVideoGameInstalls:(NSInteger)gameId andGameName:(NSString*)gameName{
+    static NSInteger count = 0;
+    count++;
+    [MobClick event:@"eventVideoGameInstalls" attributes:@{@"tap" : @"视频页面下载安装次数",@"gameName":gameName,@"gameId":[NSString stringWithFormat:@"%ld",(long)gameId]} counter:(int)count];
+}
 
 @end
